3c9fec90a88f4e61c3814186e5bcfb03ba8b36c4
Nick <niiickolay@gmail.com>
2019-12-16 21:01:50 +0200

    Replace calendar arrows with customized

--- src/components/MoviesCalendar/index.js
+++ src/components/MoviesCalendar/index.js
@@ -1,6 +1,8 @@
 import React from 'react';
 import {Calendar, LocaleConfig} from 'react-native-calendars';
 import theme from './theme';
+import NextArrow from '../../assets/img/next.svg';
+import PrevArrow from '../../assets/img/prev.svg';
 
 LocaleConfig.locales.ru = {
   monthNames: ['Январь', 'Февраль', 'Март', 'Апрель', 'Май', 'Июнь', 'Июль', 'Август', 'Сентябрь', 'Октябрь', 'Ноябрь', 'Декабрь'],
@@ -8,12 +10,21 @@ LocaleConfig.locales.ru = {
 LocaleConfig.defaultLocale = 'ru';
 
 const MoviesCalendar = ({onDayPress}) => {
+  const renderArrow = direction => {
+    return direction === 'left' ? (
+      <PrevArrow width={10} />
+    ) : (
+      <NextArrow width={10} />
+    );
+  };
+
   return (
     <Calendar
       onDayPress={onDayPress}
       monthFormat={'MMMM'}
       hideDayNames={true}
       theme={theme}
+      renderArrow={renderArrow}
     />
   );
 };
